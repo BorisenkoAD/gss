81d9cce6ea795cb25decdf8244ad079560daa644
Alexandr D. Borisenko <paz001@yandex.ru>
2016-12-30 11:11:02 +0300

    MASTER версия сайта

---
+++ feed.php
@@ -0,0 +1,50 @@
+<?php
+try 
+{
+/* $lastname = substr(htmlspecialchars(trim($_POST['lastname'])), 0, 100); */
+$name = substr(htmlspecialchars(trim($_POST['name'])), 0, 100);
+$email = substr(htmlspecialchars(trim($_POST['email'])), 0, 30);
+$text = substr(htmlspecialchars(trim($_POST['text'])), 0, 1500);
+
+$message = "Имя: $name\nИмя: \nEmail: $email\nТекст: $text\n";
+$subj = "Форма обратной связи с сайта gsst-spb.ru";
+$to = "info@gsst-spb.ru"; 
+$from="admin@gsst-spb.ru";
+$headers = "From: $from\nReply-To: $from\n";
+if (!mail($to, $subj, $message, $headers)){
+	throw new RuntimeException('Ваше сообщение не отправлено.');
+    }
+	throw new RuntimeException('Ваше сообщение отправлено.');
+} 
+catch (RuntimeException $e) {
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+	<link href="css/normalize.css" rel="stylesheet"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+	<link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin,cyrillic' rel='stylesheet' type='text/css'/>	
+    <link href="css/style.css" rel="stylesheet"/>	
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">			
+					<h2 class="text-center" style="color: #fff"><strong><?echo $e->getMessage();}?></strong></h2>
+                </div>
+            </div>
+        </div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+            setTimeout('location.replace("/feedback.html")', 2300);
+        </script>
+
+    </body>
+</html>
